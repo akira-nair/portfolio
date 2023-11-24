267e3576330aedd2d9fd7d6144529c12fc475504
akira-nair <98501964+akira-nair@users.noreply.github.com>
2023-11-23 23:20:02 -0500

    updated navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+  HStack,
+} from '@chakra-ui/react'
+import {
+    HamburgerIcon,
+    CloseIcon,
+    ChevronDownIcon,
+    ChevronRightIcon,
+    } from '@chakra-ui/icons'
+
+interface NavItem {
+    label: string
+    href?: string
+    }
+    
+const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: 'About',
+    },
+    {
+        label: 'Research',
+    },
+    {
+        label: 'Publications',
+    },
+    {
+        label: 'Contact',
+    },
+]
+
+export default function Navbar() {
+    return (
+      <Box width="100%" >
+        <Flex
+          minH={'70px'}
+          py={{ base: 2 }}
+          px={{ base: 4 }}
+          borderBottom={2}
+          borderStyle={'solid'}
+          align={'center'}
+        >
+        <HStack w={'100%'} justifyContent="space-between">
+            <Text fontSize="30" fontWeight="bold" color={'white'} letterSpacing={5}>
+                AKIRA NAIR
+            </Text>
+            <HStack spacing={20}>
+                {NAV_ITEMS.map((navItem) => (
+                <Box key={navItem.label}>
+                    <Text fontWeight="light" color={'white'}>{navItem.label}</Text>
+                </Box>
+                ))}
+            </HStack>
+        </HStack>
+        </Flex>
+      </Box>
+    );
+  }
---
+++ src/app/header.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <ul>
+        <li>Tab 1</li>
+        <li>Tab 2</li>
+        <li>Tab 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
